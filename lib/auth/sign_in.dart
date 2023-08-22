b154f1e6b27f4b7dafbb87cd81c2cc1a5cc499e1
HiroMinase <lostsymbol612@gmail.com>
2023-08-22 17:39:38 +0900

    スプラッシュスクリーン、ログイン画面の背景画像をランダムで表示できるように

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,9 +1,10 @@
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sign_in_button/sign_in_button.dart';
 
-import '../color_table.dart';
 import '../map_view.dart';
 import 'auth_service.dart';
 import 'auth_controller.dart';
@@ -20,66 +21,69 @@ class SignIn extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final Random random = Random();
+    final int backgroundImageNum = random.nextInt(28);
+
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment(-1.2, -0.5),
-            end: Alignment(2.0, 1.0),
-            stops: [0.2, 0.8],
-            colors: [
-              ColorTable.lightGradientBeginColor,
-              ColorTable.lightGradientEndColor,
-            ],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
+            Positioned.fill(
+              child: Image(
+                image: AssetImage("lib/assets/images/background/$backgroundImageNum.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
             Container(
-              margin: const EdgeInsets.symmetric(vertical: 30.0),
-              child: const Text(
-                "登録方法を選ぶ",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.black.withOpacity(0.4),
+            ),
+            Positioned(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 500),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Google
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(vertical: 20.0),
+                        child: SignInButton(
+                          Buttons.google,
+                          text: 'Google でサインイン',
+                          onPressed: () async {
+                            await ref.read(authControllerProvider).signIn(SignInMethod.google);
+
+                            final isSignIn = ref.watch(isSignedInProvider);
+                            if (isSignIn && context.mounted) {
+                              context.router.pushNamed(MapView.location);
+                            }
+                          },
+                        ),
+                      ),
+                      // Apple
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(vertical: 20.0),
+                        child: SignInButton(
+                          Buttons.apple,
+                          text: 'Apple でサインイン',
+                          onPressed: () async {
+                            await ref.read(authControllerProvider).signIn(SignInMethod.apple);
+
+                            final isSignIn = ref.watch(isSignedInProvider);
+                            if (isSignIn && context.mounted) {
+                              context.router.pushNamed(MapView.location);
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            // Google
-            Container(
-              height: 50,
-              margin: const EdgeInsets.symmetric(vertical: 10.0),
-              child: SignInButton(
-                Buttons.google,
-                text: 'Google でサインイン',
-                onPressed: () async {
-                  await ref.read(authControllerProvider).signIn(SignInMethod.google);
-
-                  final isSignIn = ref.watch(isSignedInProvider);
-                  if (isSignIn && context.mounted) {
-                    context.router.pushNamed(MapView.location);
-                  }
-                },
-              ),
-            ),
-            // Apple
-            Container(
-              height: 50,
-              margin: const EdgeInsets.symmetric(vertical: 10.0),
-              child: SignInButton(
-                Buttons.apple,
-                text: 'Apple でサインイン',
-                onPressed: () async {
-                  await ref.read(authControllerProvider).signIn(SignInMethod.apple);
-
-                  final isSignIn = ref.watch(isSignedInProvider);
-                  if (isSignIn && context.mounted) {
-                    context.router.pushNamed(MapView.location);
-                  }
-                },
               ),
             ),
           ],
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:core';
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -39,67 +41,77 @@ class SplashScreenState extends ConsumerState<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Random random = Random();
+    final int backgroundImageNum = random.nextInt(28);
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment(-1.6, -0.1),
-            end: Alignment(2.5, 1.3),
-            stops: [0.2, 0.7],
-            colors: [
-              ColorTable.lightGradientBeginColor,
-              ColorTable.lightGradientEndColor,
-            ],
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image(
+              image: AssetImage("lib/assets/images/background/$backgroundImageNum.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Text(
-              "フォトピン",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1,
-                color: ColorTable.primaryBlackColor,
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.black.withOpacity(0.4),
+          ),
+          Positioned(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "フォトピン",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                      color: ColorTable.primaryWhiteColor,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 30),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        "lib/assets/images/icon.png",
+                        width: MediaQuery.of(context).size.width / 4,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        top: BorderSide(width: 1.0, color: ColorTable.primaryWhiteColor),
+                        bottom: BorderSide(width: 1.0, color: ColorTable.primaryWhiteColor),
+                      ),
+                    ),
+                    child: const Text(
+                      "撮影スポット共有アプリ",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        height: 1.6,
+                        letterSpacing: 3,
+                        color: ColorTable.primaryWhiteColor,
+                      ),
+                      // line-heightに対して上下中央に配置するため
+                      textHeightBehavior: TextHeightBehavior(
+                        applyHeightToFirstAscent: false,
+                        applyHeightToLastDescent: false,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 30),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset(
-                  "lib/assets/images/icon.png",
-                  width: MediaQuery.of(context).size.width / 4,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-              decoration: const BoxDecoration(
-                border: Border(
-                  top: BorderSide(width: 1.0, color: ColorTable.primaryBlackColor),
-                  bottom: BorderSide(width: 1.0, color: ColorTable.primaryBlackColor),
-                ),
-              ),
-              child: const Text(
-                "撮影スポット共有アプリ",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  height: 1.6,
-                  letterSpacing: 3,
-                  color: ColorTable.primaryBlackColor,
-                ),
-                // line-heightに対して上下中央に配置するため
-                textHeightBehavior: TextHeightBehavior(
-                  applyHeightToFirstAscent: false,
-                  applyHeightToLastDescent: false,
-                ),
-              ),
-            ),
-          ]),
-        ),
+          ),
+        ],
       ),
     );
   }
